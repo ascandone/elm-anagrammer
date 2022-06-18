9437966a4bac57ac629665e11c2ed3cc5a2eec76
Alessandro Scandone <alescandone@gmail.com>
2022-06-18 21:48:23 +0200

    added validation UI to input

--- src/Ui/Input.elm
+++ src/Ui/Input.elm
@@ -4,14 +4,17 @@ module Ui.Input exposing
     , label
     , onInput
     , placeholder
+    , validation
     , value
     , view
     )
 
+import Heroicons.Outline
 import Html exposing (Html)
 import Html.Attributes exposing (class)
 import Html.Events
 import Html.Extra
+import Svg.Attributes
 
 
 type Attribute msg
@@ -38,6 +41,11 @@ label label_ =
     Attribute <| \c -> { c | label = Just label_ }
 
 
+validation : Maybe (Result String ()) -> Attribute msg
+validation validation_ =
+    Attribute <| \c -> { c | validation = validation_ }
+
+
 value : String -> Attribute msg
 value =
     attribute << Html.Attributes.value
@@ -51,6 +59,7 @@ onInput =
 type alias Config msg =
     { inputAttributes : List (Html.Attribute msg)
     , label : Maybe String
+    , validation : Maybe (Result String ())
     }
 
 
@@ -58,6 +67,7 @@ defaultConfig : Config msg
 defaultConfig =
     { inputAttributes = []
     , label = Nothing
+    , validation = Nothing
     }
 
 
@@ -78,22 +88,71 @@ view attributes =
 
         Just labelText ->
             Html.label [ class "group" ]
-                [ viewLabelContent labelText
+                [ viewLabelContent labelText config.validation
                 , viewInput config
                 ]
 
 
-viewLabelContent : String -> Html msg
-viewLabelContent labelText =
+viewLabelContent : String -> Maybe (Result String ()) -> Html msg
+viewLabelContent labelText validation_ =
     Html.span
-        [ class "ml-2 mb-1 leading-none text-gray-500 group-hover:text-indigo-500 group-focus-within:text-indigo-500" ]
+        [ class "ml-2 mb-1 leading-none"
+        , class <|
+            case validation_ of
+                Nothing ->
+                    "text-gray-500 group-hover:text-indigo-500 group-focus-within:text-indigo-500"
+
+                Just (Ok _) ->
+                    "text-green-600/90 group-hover:text-green-600 group-focus-within:text-green-600"
+
+                Just (Err _) ->
+                    "text-red-500/90 group-hover:text-red-500 group-focus-within:text-red-500"
+        ]
         [ Html.text labelText ]
 
 
 viewInput : Config msg -> Html msg
 viewInput config =
-    Html.Extra.concatAttributes Html.input
-        config.inputAttributes
-        [ class "group-hover:border-indigo-500 border px-4 py-2 transition-colors rounded-md w-full focus:ring ring-indigo-200 focus:outline-none focus:border-indigo-600 shadow-sm"
+    Html.div
+        [ class "border rounded-md focus-within:ring"
+        , class "flex items-center"
+        , class <|
+            case config.validation of
+                Nothing ->
+                    "group-hover:border-indigo-500 focus-within:border-indigo-600 ring-indigo-200"
+
+                Just (Ok _) ->
+                    "group-hover:border-green-500 focus-within:border-green-600 ring-green-200 border-green-300"
+
+                Just (Err _) ->
+                    "group-hover:border-red-500 focus-within:border-red-600 ring-red-200 border-red-200"
         ]
-        []
+        [ Html.Extra.concatAttributes Html.input
+            config.inputAttributes
+            [ class "px-4 py-2 transition-colors rounded-md  w-full  focus:outline-none  shadow-sm"
+            ]
+            []
+        , case config.validation of
+            Nothing ->
+                Html.text ""
+
+            Just result ->
+                Html.span [ class "px-2" ]
+                    [ case result of
+                        Ok _ ->
+                            viewCheckIcon
+
+                        Err _ ->
+                            viewErrorIcon
+                    ]
+        ]
+
+
+viewCheckIcon : Html msg
+viewCheckIcon =
+    Heroicons.Outline.checkCircle [ Svg.Attributes.class "h-6 w-6 text-green-500" ]
+
+
+viewErrorIcon : Html msg
+viewErrorIcon =
+    Heroicons.Outline.exclamationCircle [ Svg.Attributes.class "h-6 w-6 text-red-500" ]
